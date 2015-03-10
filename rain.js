041f3b68319faca8e619d85d2f7ef6dd8790d717
cianclarke <cian@cianclarke.com>
2015-03-10 17:06:26 -0400

    Add load runner, bugfix

--- rain.js
+++ rain.js
@@ -3,15 +3,17 @@ var request = require('request');
 var _ = require('underscore');
 var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast';
 
-// Create a new order
 app.get('/rain', function(req, res){
   var city = req.query.city, 
   country = req.query.country;
   
-  request.get({url : weatherUrl + '?q=' + city + ',' + country, json : true}, function(err, response, body){
+  request.get({url : weatherUrl + '?q=' + city + ',' + country, json : true}, function(err, response, weatherbody){
     // sum all the inches rainfall in the forecast
-    var totalRainfall = _.reduce(body.list, function(a, b){ return a + b.rain['3h'] }, 0);
-    return res.json({rainfall : totalRainfall });
+    weatherbody.rainfall = _.reduce(weatherbody.list, function(a, b){ 
+      var b = b.rain && b.rain['3h'] || 0;
+      return a + b;
+    }, 0);
+    return res.json(weatherbody);
   });
 });
 
