fa0b4bfbb8c7d077a5deff4ef7bbc64ddc33de3f
Ben Browning <bbrownin@redhat.com>
2015-06-19 13:20:37 -0400

    Don't hit any external APIs but mock them with fake delays and responses

--- rain.js
+++ rain.js
@@ -1,21 +1,30 @@
 var app = require('express')();
+var fs = require('fs');
+var jStat = require('jStat').jStat;
 var request = require('request');
 var _ = require('underscore');
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast';
+
+// hardcode some json response so we don't hit a live API
+var weatherData = JSON.parse(fs.readFileSync('weather_response.json'));
+
 app.use(require('cors')());
 
 app.get('/rain', function(req, res){
   var city = req.query.city, 
-  country = req.query.country;
+      country = req.query.country,
+      weatherbody = weatherData;
   
-  request.get({url : weatherUrl + '?q=' + city + ',' + country, json : true}, function(err, response, weatherbody){
     // sum all the inches rainfall in the forecast
     weatherbody.rainfall = _.reduce(weatherbody.list, function(a, b){ 
       var b = b.rain && b.rain['3h'] || 0;
       return a + b;
     }, 0);
+  // This will return a delay with a mean of 200ms but with a max
+  // value of 3-4 seconds
+  var delay = jStat.exponential.sample(0.005);
+  setTimeout(function() {
     return res.json(weatherbody);
-  });
+  }, delay);
 });
 
 var server = app.listen(3001);
--- sms.js
+++ sms.js
@@ -1,14 +1,17 @@
 var app = require('express')().use(require('body-parser')());
-var twilio = require('twilio');
-var client = new twilio.RestClient(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
+var jStat = require('jStat').jStat;
 app.use(require('cors')());
 // Create a new order
 app.post('/sms', function(req, res){
   var to = req.body.to, 
-  message = req.body.message;
-  client.sms.messages.create({ to: to, from : process.env.TWILIO_NUM, body : message}, function(error, messageResponse){
+      message = req.body.message;
+
+  // This will return a delay with a mean of 100ms but with a max
+  // value of 1-2 seconds
+  var delay = jStat.exponential.sample(0.01);
+  setTimeout(function() {
     return res.json(message);
-  });
+  }, delay);
 });
 
 var server = app.listen(3002);
