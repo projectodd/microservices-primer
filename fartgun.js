8c5736dee3a57ea61349cc47e653216fe5c912e8
Toby Crawley <toby@tcrawley.org>
2015-06-24 13:59:20 -0400

    A little cleanup; don't call fallback, pass it instead.

--- fartgun.js
+++ fartgun.js
@@ -7,32 +7,32 @@ module.exports = function(options) {
       circuit = new CB(options),
       run = circuit.run.bind(circuit),
       name = options.name || "foo", //TODO: better default name
+      // this doesn't watch the callback, but the action that triggers
+      // the callback
       wrap_callback = function(cb, fallback_args) {
         return function(err) {
           var fallback = function() {
             return cb.apply(null, [null].concat(fallback_args));
           };
-
-          run(function(success, failure, timeout) { // TODO: something with timeout?
+          var wrapper = function(success, failure, timeout) { // TODO: something with timeout?
             try {
               if (err) {
-                console.warn("fartgun: action for %s failed: %j", name, err);
+                console.warn("fartgun: action for '%s' failed: %j", name, err);
                 failure();
 
                 return fallback();
-              } else {
-
-                return cb.apply(null, arguments);
               }
+              success();
             } catch (e) {
-              console.error("fartgun: callback for %s failed: %j", name, e);
+              console.error("fartgun: callback for '%s' failed: %j", name, e);
 
-              return failure();
+              throw e;
             }
 
-            return success();
-          },
-              fallback());
+            return cb.apply(null, arguments);
+          };
+
+          run(wrapper, fallback);
         };
       };
 
