48d81fe6394ab9263792e4d787b14877081ddd47
Toby Crawley <toby@tcrawley.org>
2015-06-26 13:49:23 -0400

    Breaker sms and orders; only take a fn as fartgun fallback.

--- orders/api.js
+++ orders/api.js
@@ -2,8 +2,10 @@ var app = require('express')(),
     body_parser = require('body-parser')(),
     request = require('request'),
     ip = require('ip'),
+    us = require('underscore'),
     discovery = require('../lib/discovery'),
     fartgun = require('../lib/fartgun'),
+    order_circuit = fartgun({name: "orders"}),
     rain_circuit = fartgun({name: "rain",
                             onclose: function () {console.log("rain CLOSED");},
                             onopen: function () {console.log("rain OPENED");}}),
@@ -21,49 +23,49 @@ app.post('/api/orders', body_parser, function(req, res){
     return res.status(400).json({error : "Error - orders should contain a city, country, quantity and specify an account manager"});
   }
 
+  var order_cb = function(err, response, umbrellaServiceBody){
+    var sms = {to: order.accountManager,
+               message: "New order created for " +
+               order.quantity + " umbrellas!"};
+
+    var store_sms = function() {
+      //no-op, but would store the sms for future send in case of failure
+      console.error("Storing sms for recovery");
+    };
+
+    var sms_cb = function(err, response, smsServiceBody){
+      if (err) {
+        console.error("sms failed: %j", err);
+        // store message somewhere for a future resend
+        store_sms();
+      }
+    };
+
+    // 4. Generate the SMS notification
+    request.post({url: service_map.sms.url,
+                  json: sms},
+                 sms_circuit.wrap_callback(sms_cb, store_sms));
+
+    // we don't make the order contingent on notifying the mgr
+
+    // 5. All done! Back to the client
+    return res.json(order);
+  };
+
   var rain_cb = function(err, response, rainServiceBody){
     // 2. Adjust order for rainfall
     var rainfall = rainServiceBody.rainfall;
 
     order.quantity = Math.round(order.quantity * (rainfall === 0 ? 1 : rainfall));
 
-    var order_cb = function(err, response, umbrellaServiceBody){
-      if (err) {
-        console.error(err);
-        return res.status(500).json({error: err});
-      }
-
-      var sms = {to: order.accountManager,
-                 message: "New order created for " +
-                 order.quantity + " umbrellas!"};
-
-      var store_sms = function() {
-        //no-op, but would store the sms for future send in case of failure
-        console.error("Storing sms for recovery");
-      };
-
-      var sms_cb = function(err, response, smsServiceBody){
-        if (err) {
-          console.error("sms failed: %j", err);
-          // store message somewhere for a future resend
-          store_sms();
-        }
-      };
-
-      // 4. Generate the SMS notification
-      request.post({url: service_map.sms.url,
-                    json: sms},
-                   sms_circuit.wrap_callback(sms_cb, store_sms));
-
-      // we don't make the order contingent on notifying the mgr
-
-      // 5. All done! Back to the client
-      return res.json(order);
-    };
-
     // 3. Create the order in our database
     request.post({url: service_map.umbrella_orders.url,
-                  json: order}, order_cb);
+                  json: order},
+                 order_circuit.wrap_callback(order_cb,
+                                             function(err) {
+                                               console.error("Order failed: ", err);
+                                               return res.status(500).json({error: err});
+                                             }));
   };
 
   // 1. See what the rainfall will be
@@ -72,8 +74,7 @@ app.post('/api/orders', body_parser, function(req, res){
                     country: order.country},
                json: true},
               rain_circuit.wrap_callback(rain_cb,
-                                         // fallback args - passed to the callback as cb(null, {}, {rainfall: 0})
-                                         [{}, {rainfall: 0}]));
+                                         us.partial(rain_cb, null, {}, {rainfall: 0})));
 });
 
 
